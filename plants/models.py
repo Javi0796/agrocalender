5d035a6a1e4cc3148a811268f7e345a33b453ef1
Javi <javirios961107@gmail.com>
2025-12-17 16:21:36 -0500

    app plants for agrocalender web site, first version

---
+++ plants/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class PlantType(models.Model):
+    plantName = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class PlantTypeEvent(models.Model):
+    plant_type = models.ForeignKey(
+        PlantType,
+        on_delete=models.CASCADE,
+        related_name="events"
+    )
